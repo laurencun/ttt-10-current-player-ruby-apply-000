792f42038f5c1aa76858498cbd73a21ebfa79811
Lauren Cunningham <lauren.busbee@gmail.com>
2020-05-18 20:20:39 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+board = ["","","","","","","","",""]
+
+def turn_count(board)
+  counter = 0
+  turn_count.each do |value|
+    if 
+     value == "X" || value == "O"
+      counter += 1
+  end
+    return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else 
+    return "O"
+  end
+end
